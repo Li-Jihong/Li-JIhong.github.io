4b45875c25b28fc6b06439f8176abedc45080528
BH04250909 <2182726049@qq.com>
2022-09-19 22:45:09 +0800

    Site updated: 2022-09-19 22:45:09

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [];
+var precacheConfig = [["/sw-register.js","8e697836df11599a743e0fef14cf1985"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
